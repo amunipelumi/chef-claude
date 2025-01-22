c0cd7fa15b1d018fb1b8f4869d6a9ec8c5024fd9
amunipelumi <146190788+amunipelumi@users.noreply.github.com>
2025-01-22 17:52:19 +0100

    Header and body components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function () {
+  return (
+    <header>
+      <nav>
+        <img src="/chef-claude-icon.png" alt="icon" />
+        <span>Chef Claude</span>
+      </nav>
+    </header>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <main>
+      <form>
+        <input placeholder="e.g. oregano" type="text" name="" id="" />
+        <button>Add Ingredients</button>
+      </form>
+      <div>
+        {/* <h2>This is an h2</h2> */}
+      </div>
+    </main>
+  )
+}
