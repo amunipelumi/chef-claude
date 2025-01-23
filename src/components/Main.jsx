5df04463e4fe736c92c109e146bef3ed05951f98
amunipelumi <146190788+amunipelumi@users.noreply.github.com>
2025-01-23 17:10:38 +0100

    Utilizing react state for dynamic component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,47 @@
+import { useState } from 'react'
+
+// const ingredientArray = JSON.parse(localStorage.getItem('ings')) || []
+
+// const ingredientListFunc = () => {
+//   const ingredientItems = ingredientArray.map((item) => {
+//     return <li key={item}>{item}</li>
+//   })
+//   return ingredientItems
+// }
+
 export default function () {
+  const [ingredients, setIngredients] = useState([])
+  const ingredientItems = ingredients.map(item => <li key={item}>{item}</li>)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const ingredient = formData.get('ingredient')
+
+    if (ingredient.length >= 3) {
+      // ingredientArray.push(ingredient)
+      // localStorage.setItem('ings', JSON.stringify(ingredientArray))
+      // setIngredients(ingredientListFunc)
+      setIngredients(ingredients => [...ingredients, ingredient])
+    }
+  }
+
   return (
     <main>
-      <form>
-        <input placeholder="e.g. oregano" type="text" name="" id="" />
-        <button>Add Ingredients</button>
+      <form onSubmit={handleSubmit}>
+        <input  
+          type="text"
+          name="ingredient"
+          placeholder="e.g. curry"
+          aria-label="Add Ingredient"
+        />
+        <button>Add Ingredient</button>
       </form>
       <div>
-        {/* <h2>This is an h2</h2> */}
+        <h2>Ingredients:</h2>
+        <ul>
+          {ingredientItems}
+        </ul>
       </div>
     </main>
   )
