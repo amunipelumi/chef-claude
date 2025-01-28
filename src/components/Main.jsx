31ce71546bb96c95c1668443fdeb2c1bca0f915f
amunipelumi <146190788+amunipelumi@users.noreply.github.com>
2025-01-28 18:22:59 +0100

    Refactored

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import Form from './Form'
+import Ingredients from './Ingredients'
+import Recipe from './Recipe'
 
+// All functionalities to compute and render on the homepage
 export default function () {
   const [ingredients, setIngredients] = useState([])
   const ingredientItems = ingredients.map(item => <li key={item}>{item}</li>)
 
+  // Collect data from form and add to list ingredients
   const addNewIngredient = (formData) => {
     const ingredient = formData.get('ingredient')
     if (ingredient.length >= 3) {
@@ -11,30 +16,20 @@ export default function () {
     }
   }
 
+  // Rendering the recipe recieved
+  const [recipeShown, setRecipeShown] = useState(false)
+  const showRecipe = () => {
+    setRecipeShown(prev => !prev)
+  }
+
   return (
     <main>
-      <form action={addNewIngredient}>
-        <input  
-          type="text"
-          name="ingredient"
-          placeholder="e.g. curry"
-          aria-label="Add Ingredient"
-        />
-        <button>Add Ingredient</button>
-      </form>
-      <section>
-        <h2>Ingredients on hand:</h2>
-        <ul aria-live="polite">
-          {ingredientItems}
-        </ul>
-        <div className="get-recipe-container">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button>Get a recipe</button>
-        </div>
-      </section>
+      <Form addNewIngredient={addNewIngredient}/>
+      {ingredients.length > 0 && <Ingredients 
+        listItems={ingredientItems}
+        ingredients={ingredients}
+        show={showRecipe}/>}
+      {recipeShown ? <Recipe /> : null}
     </main>
   )
 }
