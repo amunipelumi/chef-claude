d11e72b44a3d6a3bb19e6af3c94611c513f45038
amunipelumi <146190788+amunipelumi@users.noreply.github.com>
2025-01-29 19:55:11 +0100

    Refactored ingredients component

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,16 +1,20 @@
 export default function (props) {
+
+  const { ingredients, getRecipe } = props
+  const ingredientItems = ingredients.map(item => <li key={item}>{item}</li>)
+
   return (
     <section>
       <h2>Ingredients on hand:</h2>
       <ul aria-live="polite">
-        {props.listItems}
+        {ingredientItems}
       </ul>
-      {props.ingredients.length > 3 && <div className="get-recipe-container">
+      {ingredients.length > 3 && <div className="get-recipe-container">
         <div>
           <h3>Ready for a recipe?</h3>
           <p>Generate a recipe from your list of ingredients.</p>
         </div>
-        <button onClick={props.show}>Get a recipe</button>
+        <button onClick={getRecipe}>Get a recipe</button>
       </div>}
     </section>
   )
